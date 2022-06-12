0d7254e0dd62cea79416fa555622bc64ea12fe3b
chiradeep <chiradeep13@gmail.com>
2022-06-13 03:25:28 +0530

    [ADD] both frontend and backed

--- backend/djangoProject/urls.py
+++ backend/djangoProject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('/',include('playground.paths')),
     path('admin/', admin.site.urls),
     path('playground/',include('playground.paths')),
 ]
---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      message: 'Hello Vue.js!'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
